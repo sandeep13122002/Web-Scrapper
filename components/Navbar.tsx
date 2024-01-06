a47d38146b646b1d93a8256ea12d7d3d46909405
sandeep <sandykumaryadav70@gmail.com>
2024-01-06 12:03:35 +0530

    project setup

---
+++ components/Navbar.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className="w-full">
+      <nav className="nav" >
+       <Link href="/"    className="flex items-center gap-1">
+        <Image src="/assets/icons/logo.svg"
+        width={27}
+        height={27}
+        alt="logo"
+        />
+
+        <p className="nav-logo">
+
+          Price<span className='text-primary'>wise</span>
+        </p>
+       </Link>
+      </nav>
+
+
+
+
+    </header>
+  )
+}
+
+export default Navbar
