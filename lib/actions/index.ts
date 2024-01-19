db9ca7c2880e22d6e2f18256c2c8e166d25cc16f
sandeep <sandykumaryadav70@gmail.com>
2024-01-19 18:18:26 +0530

    database connection completed

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,8 +1,10 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
 import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 
 
@@ -17,6 +19,35 @@ try{
 
          let product=scrapedProduct;
          const existingProduct=await Product.findOne({url:scrapedProduct.url});
+
+        if(existingProduct){
+            const updatedPriceHistory : any=[
+                ...existingProduct.priceHistory,
+                {price:scrapedProduct.currentPrice}
+            ]
+
+         product={
+            ...scrapedProduct,
+            priceHistory:updatedPriceHistory,
+            lowestPrice:getLowestPrice(updatedPriceHistory),
+            highestPrice:getHighestPrice(updatedPriceHistory),
+            averagePrice:getAveragePrice(updatedPriceHistory),
+
+         }
+}
+
+
+const newProduct=await Product.findOneAndUpdate(
+  {url:scrapedProduct.url},
+  product,
+  {upsert:true,new:true}  
+);
+
+revalidatePath(`/products/${newProduct._id}`);
+
+
+
+
 
 }catch(error:any){
     throw new Error(`Failed to create/update product: ${error.message}`)
