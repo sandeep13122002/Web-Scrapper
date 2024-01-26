e26793ccb4bd03b724513a570921f192a17e07df
sandeep <sandykumaryadav70@gmail.com>
2024-01-26 11:38:34 +0530

    email notification feature added

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -163,7 +163,7 @@ const ProductDetails = async ({params:{id}}:Props) => {
 
 
 </div>
-  <Modal/>
+  <Modal productId={id}/>
 
           </div>
 
--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -5,6 +5,8 @@ import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { User } from "@/types";
+import { generateEmailBody, sendEmail } from "../nodemailer";
 
 
 
